5953f16c46ee8d8be4d94597c228a3f734406aed
Vinayyad9897 <vinayyadav91190@gmail.com>
2024-03-08 10:51:57 +0530

    initial commit

---
+++ src/main/java/com/social/FullstacklearningApplication.java
@@ -0,0 +1,13 @@
+package com.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstacklearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstacklearningApplication.class, args);
+	}
+
+}
